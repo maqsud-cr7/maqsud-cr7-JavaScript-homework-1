88d194775e035cff30a5bec4c13627336f79a05c
unknown <maqsud11rashidov05@gmail.com>
2020-10-23 17:08:50 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = prompt('Введите свое имя');
+console.log("Меня зовут " + name);
+var age = prompt('Введите свой возраст');
+console.log("Мне " + age);
+var task1 = prompt('Решите пример 6 + 3 ?');
+console.log("Пример 1: 6 + 3 = 9 Ваш ответ " + task1);
+var task2 = prompt('Решите пример 20 - 5 ?');
+console.log("Пример 2: 20 - 5 = 15 Ваш ответ " + task2);
+var task3 = prompt('Решите пример 15 * 2 ?');
+console.log("Пример 3: 15 * 2 = 30 Ваш ответ " + task3);
+var task4 = prompt('Решите пример 8 / 4 ?');
+console.log("Пример 4: 8 / 4 = 2 Ваш ответ " + task4);
+var task5 = prompt('Решите пример 10 % 3 ?');
+console.log("Пример 5: 10 % 3 = 1  Ваш ответ " + task5);
+alert('Откройте консоль для просмотра');
